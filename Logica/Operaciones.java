aa611d2660cc2243556e2f303ced74a1f9372fcc
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2020-09-27 17:19:40 -0500

    Update Operaciones.java

--- Logica/Operaciones.java
+++ Logica/Operaciones.java
@@ -1,1 +1,31 @@
+package Logica;
+
+public class Operaciones {
+        
+    public int suma (int n1, int n2){
+        int resul = 0;
+        resul = n1 + n2;
+        return resul;
+    }
+    
+    public int resta (int n1, int n2){
+        int resul = 0;
+        resul = n1 - n2;
+        return resul;
+    }
+    
+    public int multiplicacion (int n1, int n2){
+        int resul = 0;
+        resul = n1 * n2;
+        return resul;
+        
+    }
+    
+    public double division (double n1, double n2){
+        double resul = 0; 
+        resul = n1 / n2;
+        return resul;
+    }
+    
+}
 
